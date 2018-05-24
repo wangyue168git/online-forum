a7b278fcba20c6492a3841d52cfb3e649f01b3c5
wangyue168git <973459080@qq.com>
2018-05-24 19:41:56 +0800

    统一优先级表

--- src/main/java/com/bolo/crawler/queue/ScheduleQueueManager.java
+++ src/main/java/com/bolo/crawler/queue/ScheduleQueueManager.java
@@ -1,11 +1,19 @@
 package com.bolo.crawler.queue;
 
+import java.util.TreeMap;
+import java.util.concurrent.atomic.LongAdder;
+
 /**
  * @Author wangyue
  * @Date 18:08
  */
 public class ScheduleQueueManager {
     private static final ScheduleQueue scheduleQueue = new ScheduleQueue();
+    private static final TreeMap<Long, LongAdder> urlMap = new TreeMap<>();
+
+    public static TreeMap<Long, LongAdder> getUrlMap() {
+        return urlMap;
+    }
     public static final ScheduleQueue getScheduleQueue(){
         return scheduleQueue;
     }
