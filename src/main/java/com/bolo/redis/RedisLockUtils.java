999e6094cf9a3334a5e0dcbfd784db88c13c2add
wangyue79 <wangyue79@maoyan.com>
2018-10-31 11:05:03 +0800

    分布式🔒

--- src/main/java/com/bolo/redis/RedisLockUtils.java
+++ src/main/java/com/bolo/redis/RedisLockUtils.java
@@ -4,10 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisStringCommands;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.types.Expiration;
+import redis.clients.jedis.Jedis;
 
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.UUID;
 
@@ -17,61 +21,45 @@ import java.util.UUID;
  * @Date 16:44
  */
 public class RedisLockUtils {
-    private static final Logger log = LoggerFactory.getLogger(RedisLockUtils.class);
 
-    public RedisLockUtils() {
+    private static final Logger log = LoggerFactory.getLogger(RedisLockUtils.class);
+    private static final String LOCK_SUCCESS = "OK";
+    private static final String SET_IF_NOT_EXIST = "NX";
+    private static final String SET_WITH_EXPIRE_TIME = "PX";
+
+    private RedisLockUtils() {
     }
 
     private static boolean isEmpty(CharSequence cs) {
         return cs == null || cs.length() == 0;
     }
 
-    public static Boolean tryLock(RedisTemplate jedisTemplate, String lockName, int lockTimeout) {
-        return tryLock(jedisTemplate, lockName, lockTimeout, 5);
+    public static Boolean tryLock(Jedis jedis, String lockName,String identifier,  int lockTimeout) {
+        return tryLock(jedis, lockName,identifier,lockTimeout, 5);
     }
 
-    public static Boolean tryLock(RedisTemplate jedisTemplate, String lockName, int lockTimeout, int acquireTimeout) {
-        return tryLock(jedisTemplate, lockName, lockTimeout, acquireTimeout, 1000L);
+    public static Boolean tryLock(Jedis jedis, String lockName,String identifier,  int lockTimeout, int acquireTimeout) {
+        return tryLock(jedis, lockName, identifier,lockTimeout,acquireTimeout, 1000L);
     }
 
-    public static Boolean tryLock(final RedisTemplate jedisTemplate, String lockName, final int lockTimeout, int acquireTimeout, long retryDuration) {
+    public static Boolean tryLock(Jedis jedis, String lockName,String identifier,  int lockTimeout, int acquireTimeout, long retryDuration) {
         if (!isEmpty(lockName) && lockTimeout > 0) {
-            final String lockKey = lockName;
-            final String identifier = UUID.randomUUID().toString();
-            Calendar atoCal = Calendar.getInstance();
-            atoCal.add(13, acquireTimeout);
-            Date atoTime = atoCal.getTime();
+            Long endTime = System.currentTimeMillis() + acquireTimeout*1000L;
             log.info("Try to acquire the lock. lockKey={},acquireTimeout={}s,lockTimeout={}s", new Object[]{lockName, acquireTimeout, lockTimeout});
-
             do {
-                boolean acquiredLock = ((Boolean)jedisTemplate.execute(new RedisCallback<Boolean>() {
-                    public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
-                        return connection.setNX(jedisTemplate.getStringSerializer().serialize(lockKey), jedisTemplate.getStringSerializer().serialize(identifier));
-                    }
-                })).booleanValue();
-                //如果程序在此时奔溃，会导致没办法设置锁时间，导致其他人无法获取锁
-                if (acquiredLock) {
-                    jedisTemplate.execute(new RedisCallback<Boolean>() {
-                        public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
-                            return connection.expire(jedisTemplate.getStringSerializer().serialize(lockKey), (long)lockTimeout);
-                        }
-                    });
-                    log.info("acquired lock. lockKey={}", lockKey);
+                String acquiredLock = jedis.set(lockName, identifier, SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, lockTimeout);
+                if (LOCK_SUCCESS.equals(acquiredLock)) {
+                    log.info("acquired lock. lockKey={}", lockName);
                     return true;
                 }
-
-                log.info("Retry to acquire the lock. lockKey={},acquireTimeout={}s,lockTimeout={}s", new Object[]{lockKey, acquireTimeout, lockTimeout});
-                if (acquireTimeout < 0) {
-                    return false;
-                }
-
+                log.info("Retry to acquire the lock. lockKey={},acquireTimeout={}s,lockTimeout={}s", new Object[]{lockName, acquireTimeout, lockTimeout});
                 try {
-                    log.info("wait 1000 milliseconds before retry. lockKey={}", lockKey);
+                    log.info("wait 1000 milliseconds before retry. lockKey={}", lockName);
                     Thread.sleep(retryDuration);
-                } catch (InterruptedException var12) {
+                } catch (InterruptedException ignored) {
                     ;
                 }
-            } while(!(new Date()).after(atoTime));
+            } while(System.currentTimeMillis() < endTime);
 
             return false;
         } else {
@@ -79,17 +67,10 @@ public class RedisLockUtils {
         }
     }
 
-    public static void unLock(final RedisTemplate jedisTemplate, final String lockName,final String userTag) {
-        if (!isEmpty(lockName)) {
-            jedisTemplate.execute(new RedisCallback<Void>() {
-                public Void doInRedis(RedisConnection connection) throws DataAccessException {
-                    String str = String.valueOf(connection.get(jedisTemplate.getStringSerializer().serialize(lockName)));
-                    if (str.equals(userTag)) {
-                        connection.del(new byte[][]{jedisTemplate.getStringSerializer().serialize(lockName)});
-                    }
-                    return null;
-                }
-            });
+    public static void unLock(Jedis jedis,  String lockName, String userTag) {
+        if (!isEmpty(lockName) && !isEmpty(userTag)) {
+            String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+            jedis.eval(script, Collections.singletonList(lockName), Collections.singletonList(userTag));
         }
     }
 }
